ea3e78b382dc6c51f780003d5a6ec690a8f04139
Eva <eva.araratyan@student.rau.am>
2026-03-01 15:47:48 +0400

    art_api

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://art_user:art_pass@localhost:5432/art_db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
